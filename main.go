b0d2073d3ce05204db540b4af6335f5dcd1a6c53
Hector Kemp <hkemp@redhat.com>
2023-10-09 09:37:44 +1100

    gpt attempt 2

--- main.go
+++ main.go
@@ -1,81 +1,120 @@
 package main
 
 import (
-	// "context"
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
-	// "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-// getKubeConfigPath retrieves the path to the kubeconfig file.
 func getKubeConfigPath() string {
 	home := os.Getenv("HOME")
 	return filepath.Join(home, ".kube", "config")
 }
 
-// getKubeClient gets a Kubernetes client using the local kubeconfig.
-func getKubeClient() (*kubernetes.Clientset, error) {
+func getKubeClients() (*kubernetes.Clientset, dynamic.Interface, error) {
 	config, err := clientcmd.BuildConfigFromFlags("", getKubeConfigPath())
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return clientset, nil
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+	return clientset, dynamicClient, nil
 }
 
-// FetchResourcesForAPIGroup retrieves resources for a specific API group and namespace.
-func FetchResourcesForAPIGroup(client *kubernetes.Clientset, namespace string, apiGroup string) ([]string, error) {
+func DiscoverAPIGroups(client *kubernetes.Clientset) ([]string, error) {
+	groups, err := client.Discovery().ServerGroups()
+	if err != nil {
+		return nil, err
+	}
+
+	var apiGroups []string
+	for _, group := range groups.Groups {
+		for _, version := range group.Versions {
+			apiGroups = append(apiGroups, version.GroupVersion)
+		}
+	}
+	return apiGroups, nil
+}
+
+func FetchObjectsForAPIGroup(client *kubernetes.Clientset, dynamicClient dynamic.Interface, namespace string, apiGroup string) ([]string, error) {
+	fmt.Println(apiGroup)
 	resourceList, err := client.Discovery().ServerResourcesForGroupVersion(apiGroup)
+	fmt.Println(resourceList)
 	if err != nil {
 		return nil, err
 	}
 
-	var resources []string
+	var objects []string
 	for _, apiResource := range resourceList.APIResources {
-		resources = append(resources, apiResource.Name)
-	}
-	return resources, nil
-}
+		gvr := schema.GroupVersionResource{
+			Group:    apiResource.Group,
+			Version:  apiResource.Version,
+			Resource: apiResource.Name,
+		}
 
-// GetResourcesMap retrieves a hierarchical map of API groups to resources.
-func GetResourcesMap(namespace string, apiGroups []string) (map[string][]string, error) {
-	client, err := getKubeClient()
-	if err != nil {
-		return nil, err
-	}
-
-	result := make(map[string][]string)
-	for _, apiGroup := range apiGroups {
-		resources, err := FetchResourcesForAPIGroup(client, namespace, apiGroup)
+		fmt.Println(gvr)
+		objs, err := dynamicClient.Resource(gvr).Namespace(namespace).List(context.TODO(), v1.ListOptions{})
+		fmt.Println(objs)
 		if err != nil {
 			return nil, err
 		}
-		result[apiGroup] = resources
+		for _, obj := range objs.Items {
+			objects = append(objects, obj.GetName())
+		}
+	}
+	return objects, nil
+}
+
+func GetResourcesMap(namespace string, apiGroups []string, client *kubernetes.Clientset, dynamicClient dynamic.Interface) (map[string][]string, error) {
+	fmt.Println(apiGroups)
+	result := make(map[string][]string)
+	for _, apiGroup := range apiGroups {
+		objects, err := FetchObjectsForAPIGroup(client, dynamicClient, namespace, apiGroup)
+		if err != nil {
+			return nil, err
+		}
+		result[apiGroup] = objects
 	}
 	return result, nil
 }
 
 func main() {
-	apiGroups := []string{"apps/v1", "v1"}
-	namespace := "default"
+	client, dynamicClient, err := getKubeClients()
+	if err != nil {
+		fmt.Println("Error fetching client:", err)
+		return
+	}
 
-	resourcesMap, err := GetResourcesMap(namespace, apiGroups)
+	apiGroups, err := DiscoverAPIGroups(client)
+	if err != nil {
+		fmt.Println("Error discovering API groups:", err)
+		return
+	}
+
+	namespace := "default"
+	resourcesMap, err := GetResourcesMap(namespace, apiGroups, client, dynamicClient)
 	if err != nil {
 		fmt.Println("Error fetching resources:", err)
 		return
 	}
 
-	for group, resources := range resourcesMap {
+	for group, objects := range resourcesMap {
 		fmt.Println(group)
-		for _, resource := range resources {
-			fmt.Printf("  - %s\n", resource)
+		for _, object := range objects {
+			fmt.Printf("  - %s\n", object)
 		}
 	}
 }
